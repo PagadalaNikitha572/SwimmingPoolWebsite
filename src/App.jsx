2de344fe58b95bb6247b593c02c7cab917e5588c
PagadalaNikitha572 <147132859+PagadalaNikitha572@users.noreply.github.com>
2026-01-28 23:15:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/pool-equipment-site">
+    <Router>
       <div className="app">
         <Navbar />
         <main className="main-content">
